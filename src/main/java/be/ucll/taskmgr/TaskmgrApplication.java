80b65b5e5ae5041ed1163d1da2da5b3f6c92b8ff
Stef Tweepenninckx <stef.tweepenninckx@gmail.com>
2020-05-29 14:27:15 +0200

    initial commit

---
+++ src/main/java/be/ucll/taskmgr/TaskmgrApplication.java
@@ -0,0 +1,13 @@
+package be.ucll.taskmgr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmgrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskmgrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/ucll/taskmgr/TaskmgrApplicationTests.java
@@ -0,0 +1,13 @@
+package be.ucll.taskmgr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskmgrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
